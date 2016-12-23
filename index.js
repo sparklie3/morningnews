9d29ba554a8ca6ceb641389c56bbf689132e048e
Shaw <shaw.s.li@gmail.com>
2016-12-23 05:47:56 +0000

    testing to see if speech works

--- index.js
+++ index.js
@@ -22,73 +22,82 @@ function endIt(res, data){
 
 //example ?source=the-next-web&sortBy=latest&apiKey=507a41fc68104d1484c8eabdefffce42
 
-app.post('/',function(req,res){
-   res.send(req);
-   res.end();
-   
-   
-   /*
-   {
-    "speech": "Barack Hussein Obama II is the 44th and current President of the United States.",
-    "displayText": "Barack Hussein Obama II is the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where ",
-    "data": {...},
-    "contextOut": [...],
-    "source": "DuckDuckGo"
-    }
-    */
-});
-
-
-app.get('/', function(req,res){
-    return new Promise(function(resolve, reject){
-        var category = "business"; // this is the input from api.ai
-        request.get(newsSource+categoryMethod+category, function(error, response, data){
-             if (!error && response.statusCode == 200){
-                 data = JSON.parse(data);
-                 //console.log(data.sources.length);
-                 var getSourceId = function(data){
-                     var sourceId = [];
-                     for (var i =0; i<data.sources.length; i++){
-                       sourceId.push(data.sources[i].id);
-                    }
-                     return sourceId;
-                 };
-                var sourceId = getSourceId(data);
-                var randNumber = Math.round(Math.random()*sourceId.length);
-                //console.log(newsArticle+sourceMethod+sourceId[randNumber]);
-                request.get(newsArticle+sourceMethod+sourceId[randNumber],function(error, response, data){
-                    if (!error && response.statusCode == 200){
-                        data = JSON.parse(data);
-                        //console.log(data);
-                        //console.log(data.articles[0].description);
+app.route('/')
+    .get(function(req,res){
+        return new Promise(function(resolve, reject){
+            var category = "business"; // this is the input from api.ai
+            request.get(newsSource+categoryMethod+category, function(error, response, data){
+                 if (!error && response.statusCode == 200){
+                     data = JSON.parse(data);
+                     //console.log(data.sources.length);
+                     var getSourceId = function(data){
+                         var sourceId = [];
+                         for (var i =0; i<data.sources.length; i++){
+                           sourceId.push(data.sources[i].id);
+                        }
+                         return sourceId;
+                     };
+                    var sourceId = getSourceId(data);
+                    var randNumber = Math.round(Math.random()*sourceId.length);
+                    //console.log(newsArticle+sourceMethod+sourceId[randNumber]);
+                    request.get(newsArticle+sourceMethod+sourceId[randNumber],function(error, response, data){
+                        if (!error && response.statusCode == 200){
+                            data = JSON.parse(data);
+                            //console.log(data);
+                            //console.log(data.articles[0].description);
+                            
+                            
+                            res.send(data.articles[0].description);
+                            res.end();
+                        }else{
+                            console.log(response);
+                            console.log(error);
+                            throw error;
+                        }
                         
                         
-                        res.send(data.articles[0].description);
-                        res.end();
-                    }else{
-                        console.log(response);
-                        console.log(error);
-                        throw error;
-                    }
+                    })   ;      
+                     
                     
-                    
-                })   ;      
+                 }else{
+                     console.log(error);
+                     throw error;
+                 }
                  
-                
-             }else{
-                 console.log(error);
-                 throw error;
-             }
-             
-        })
+            })
+            
+            
+        });
         
         
-    });
     
+           
+    })
     
-
+    .post(function(req,res){
+        var output =   {
+            "speech": "Barack Hussein Obama II is the 44th and current President of the United States.",
+            "displayText": "Barack Hussein Obama II is the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where ",
+            "data": {},
+            "contextOut": [],
+            "source": "DuckDuckGo"
+            }
        
-});
+       res.json(output);
+       res.end();
+       
+       
+       /*
+       {
+        "speech": "Barack Hussein Obama II is the 44th and current President of the United States.",
+        "displayText": "Barack Hussein Obama II is the 44th and current President of the United States, and the first African American to hold the office. Born in Honolulu, Hawaii, Obama is a graduate of Columbia University   and Harvard Law School, where ",
+        "data": {...},
+        "contextOut": [...],
+        "source": "DuckDuckGo"
+        }
+        */
+    });
+            
 
 
 app.listen(process.env.PORT, function(){
